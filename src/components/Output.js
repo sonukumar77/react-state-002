f5b0f8480ae1f0dca677760df137539b0c7c4524
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2022-11-07 21:19:45 +0530

    Update Output.js

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,11 +1,15 @@
 import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
 const Output = (props) => {
+
+    const markdown = `${props.data}`;
 
     return (
         <>
             <div className="outputBox">
                 <h2 className='header2'>Output</h2>
-                <ReactMarkdown className="output">{props.data}</ReactMarkdown>
+                <ReactMarkdown className="output" children={markdown} remarkPlugins={[remarkGfm]} />
             </div>
         </>
     )
